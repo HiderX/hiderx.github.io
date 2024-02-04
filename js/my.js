e1e2b79e9da9b8b033e9c3bc465aec43f28fc0cc
hiderx <3193997905@qq.com>
2024-02-04 10:15:31 +0800

    deploy, heads/main-0-geb9d9dc

---
+++ js/my.js
@@ -0,0 +1,8 @@
+window.onfocus = function () {
+    console.log("onfocus");
+    document.title = "哔哩哔哩 - ( ゜- ゜)つロ 干杯~";
+};
+window.onblur = function () {
+    console.log("onblur");
+    document.title = "怎么离开了QAQ";
+}
